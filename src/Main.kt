1a320e727fc5749d35e08dd57dcfc28c58fa43b3
357-22 <vika.p.2006@bk.ru>
2024-10-26 09:50:47 +0300

    Готовое решение

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val array = Array(5) { IntArray(5) }
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    println("Введите 25 целых чисел для массива 5x5:")
+    for (i in 0 until 5) {
+        for (j in 0 until 5) {
+            print("Элемент [$i][$j]: ")
+            array[i][j] = readLine()!!.toInt()
+        }
+    }
+
+    var a = true
+    for (i in 0 until 5) {
+        for (j in 0 until 5) {
+            if (array[i][j] != array[j][i]) {
+                a = false
+                break
+            }
+        }
+        if (!a) break
+    }
+    if (a) {
+        println("Массив симметричен относительно главной диагонали.")
+    } else {
+        println("Массив не симметричен относительно главной диагонали.")
     }
 }
